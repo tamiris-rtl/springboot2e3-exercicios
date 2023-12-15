3e13ac72eba95bc8986c61624e2a8b17e6710a45
tamiris-rtl <146030970+tamiris-rtl@users.noreply.github.com>
2023-12-15 19:09:53 -0300

    Update ApiApplication.java

--- src/main/java/med/voll/api/ApiApplication.java
+++ src/main/java/med/voll/api/ApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiApplication {
-
+// aaaa
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
